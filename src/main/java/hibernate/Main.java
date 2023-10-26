18a083bf3a4b5869d35bd723b8ac757480452633
Stepanova Anastasia <56322703+stepanovaaa@users.noreply.github.com>
2023-10-26 15:52:01 +0400

    Hibernate API(получения диничного объекта по ID)

--- src/main/java/hibernate/Main.java
+++ src/main/java/hibernate/Main.java
@@ -1,8 +1,8 @@
 package hibernate;
 
+import hibernate.entity.User;
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
-import org.hibernate.query.NativeQuery;
 
 @Log4j2
 public class Main {
@@ -11,22 +11,9 @@ public class Main {
         // получаем готовый Session Factory и сразу создаем сессию
         Session session = HibernateUtil.getSessionFactory().openSession();
 
-        String qry = "select " +
-                "    count(u.id), " +
-                "    substring(u.email, position('@' in u.email)+1, length(u.email)) as domainmail " +
-                "from todolist.user_data u " +
-                "where u.email like '%@%' " +
-                "group by substring(u.email, position('@' in u.email)+1, length(u.email))";
-        NativeQuery<Object[]> sqlQuery = session.createNativeQuery(qry, Object[].class);
+        User user = session.get(User.class, 1);
 
-        log.info(sqlQuery.getResultList());
-
-        for(Object obj: sqlQuery.getResultList()){
-            Object[] objArray = (Object[]) obj;
-            log.info(objArray[0]);
-            log.info(objArray[1]);
-            log.info("------");
-        }
+        log.info(user);
 
         session.close(); // закрыть сессию
 
