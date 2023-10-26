962597e8686d86d62747ca326b14610c88e829fc
Stepanova Anastasia <56322703+stepanovaaa@users.noreply.github.com>
2023-10-26 13:52:42 +0400

    NativeSQL(select)

--- src/main/java/hibernate/Main.java
+++ src/main/java/hibernate/Main.java
@@ -1,23 +1,26 @@
 package hibernate;
 
 import hibernate.entity.User;
-import jakarta.persistence.Query;
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 
 import java.util.List;
 
 @Log4j2
 public class Main {
     public static void main(String[] args) {
-        log.info("tutorials start");
+//        log.info("tutorials start");
         // получаем готовый Session Factory и сразу создаем сессию
         Session session = HibernateUtil.getSessionFactory().openSession();
 
-        Query query = session.createQuery("From User", User.class);
-        List<User> users = query.getResultList();
+        String qry = "select * from todolist.user_data";
+        NativeQuery sqlQuery = session.createNativeQuery(qry, User.class);
+        sqlQuery.addEntity(User.class);
+        sqlQuery.setMaxResults(10);
 
-        log.info(users);
+        List<User> list = sqlQuery.list();
+        log.info(list);
 
         session.close(); // закрыть сессию
 
