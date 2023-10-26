36a695c1b20b4f0293d890dab63721e0823d6c5c
Stepanova Anastasia <56322703+stepanovaaa@users.noreply.github.com>
2023-10-26 13:11:20 +0400

    HQL(select)

--- src/main/java/hibernate/Main.java
+++ src/main/java/hibernate/Main.java
@@ -1,12 +1,11 @@
 package hibernate;
 
 import hibernate.entity.User;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaUpdate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.Query;
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
+
+import java.util.List;
 
 @Log4j2
 public class Main {
@@ -14,40 +13,11 @@ public class Main {
         log.info("tutorials start");
         // получаем готовый Session Factory и сразу создаем сессию
         Session session = HibernateUtil.getSessionFactory().openSession();
-//        // подготовка запроса - полцчение всех пользователей
-//        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-//        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
-//        Root<User> root = criteriaQuery.from(User.class);
-//        criteriaQuery.select(root); // конечный запрос select из таблицы User
-//
-//        Predicate p1 = criteriaBuilder.gt(root.get("id"), 1000);
-//        Predicate p2 = criteriaBuilder.lt(root.get("id"), 1005);
-//
-//        criteriaQuery.select(root).where(criteriaBuilder.and(p1,p2));
-//
-//        // Выполнение запроса с получением результата
-//        Query query = session.createQuery(criteriaQuery);
-//        List<User> users = query.getResultList();
 
-//        CriteriaBuilder cb = session.getCriteriaBuilder();
-//        CriteriaDelete<User> criteriaDelete = cb.createCriteriaDelete(User.class);
-//        Root<User> root = criteriaDelete.from(User.class);
-//        criteriaDelete.where(cb.equal(root.get("id"),5003));
-//
-//        Transaction transaction = session.beginTransaction();
-//        session.createQuery(criteriaDelete).executeUpdate();
-//        transaction.commit();
+        Query query = session.createQuery("From User", User.class);
+        List<User> users = query.getResultList();
 
-        CriteriaBuilder cb = session.getCriteriaBuilder();
-        CriteriaUpdate<User> criteriaUpdate = cb.createCriteriaUpdate(User.class);
-        Root<User> root = criteriaUpdate.from(User.class);
-
-        criteriaUpdate.set("email", "sgoihdfugh@gmail.ru");
-        criteriaUpdate.where(cb.equal(root.get("id"), 3));
-
-        Transaction transaction = session.beginTransaction();
-        session.createQuery(criteriaUpdate).executeUpdate();
-        transaction.commit();
+        log.info(users);
 
         session.close(); // закрыть сессию
 
