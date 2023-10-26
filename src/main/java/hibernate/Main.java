7f555ea9130714c28dd9955204d5e41f6fc0a1a9
Stepanova Anastasia <56322703+stepanovaaa@users.noreply.github.com>
2023-10-26 12:15:07 +0400

    Revert "Criteria API"
    This reverts commit d7d9607708d8fe33e3637f50041d663a93c3e6bb.

--- src/main/java/hibernate/Main.java
+++ src/main/java/hibernate/Main.java
@@ -1,33 +1,27 @@
 package hibernate;
 
-import hibernate.entity.User;
-import jakarta.persistence.Query;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
 import lombok.extern.log4j.Log4j2;
+import hibernate.entity.User;
 import org.hibernate.Session;
-
-import java.util.List;
-
 @Log4j2
 public class Main {
     public static void main(String[] args) {
         log.info("tutorials start");
-        // получаем готовый Session Factory и сразу создаем сессию
+        // получаем готовый и сразу создаем сессию
         Session session = HibernateUtil.getSessionFactory().openSession();
-        // подготовка запроса - полцчение всех пользователей
-        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-        CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
-        Root<User> root = criteriaQuery.from(User.class);
-        criteriaQuery.select(root); // конечный запрос select из таблицы User
+        // открыть транзакцию
+        session.getTransaction().begin();
 
-        // Выполнение запроса с получением результата
-        Query query = session.createQuery(criteriaQuery);
-        List<User> users = query.getResultList();
+        User user = new User(); // состояние NEW (transient)
+        user.setEmail("newfrommap1@mail.com");
+        user.setUsername("newfrom1");
+        user.setPassword("s;klgh;jgh");
 
-        session.close(); // закрыть сессию
+        session.persist(user); // // состояние managed (persistent - управляемый объект - контейнером)
 
-        HibernateUtil.close(); // закрыть Session Factory
+        session.getTransaction().commit(); // сохранить изменения
+        session.close(); // закрыть транзакцию
+        // detached
+        HibernateUtil.close();
     }
 }
